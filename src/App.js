9d2eeb7b620af78754d5aae1fd7823ba8c3ff719
Marianne Feng <feng.marianne@gmail.com>
2019-05-13 00:06:46 -0700

    moved menu out to its own component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Root, Routes, addPrefetchExcludes, withSiteData } from 'react-static'
 //
 import { Link, Router } from 'components/Router'
 import Dynamic from 'containers/Dynamic'
+import Menu from "./components/Menu";
 
 // Any routes that start with 'dynamic' will be treated as non-static routes
 addPrefetchExcludes(['dynamic'])
@@ -11,27 +12,7 @@ function App(siteData) {
   //TODO: make mobile collapse/uncollapse menu work
   return (
     <Root>
-        <nav className="navbar" role="navigation" aria-label="main navigation">
-            <div className="navbar-brand">
-                <Link className="navbar-item" to="/">
-                    <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
-                </Link>
-
-                <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false"
-                   data-target="navbarBasicExample">
-                    {siteData.content.map((c, i) => (<span key={'mobileMenu' + i} aria-hidden="true">{c.label}</span>))}
-                </a>
-            </div>
-
-            <div id="navbarBasicExample" className="navbar-menu">
-                <div className="navbar-end">
-                    {siteData.content.map((c, i) => (
-                        <Link key={'desktopMenu' + i} className="navbar-item" to={c.path}>{c.label}</Link>
-                    ))}
-                </div>
-            </div>
-        </nav>
-
+      <Menu content={siteData.content} />
       <div className="content">
         <React.Suspense fallback={<em>Loading...</em>}>
           <Router>
---
+++ src/components/Menu.js
@@ -0,0 +1,29 @@
+import {withSiteData} from "react-static";
+import React from 'react'
+import { Link } from 'components/Router'
+
+//TODO: code review - finally settle once and for all how we want to declare React components
+export default (props) => {
+    return (
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+            <div className="navbar-brand">
+                <Link className="navbar-item" to="/">
+                    <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/>
+                </Link>
+
+                <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false"
+                   data-target="navbarBasicExample">
+                    {props.content.map((c, i) => (<span key={'mobileMenu' + i} aria-hidden="true">{c.label}</span>))}
+                </a>
+            </div>
+
+            <div id="navbarBasicExample" className="navbar-menu">
+                <div className="navbar-end">
+                    {props.content.map((c, i) => (
+                        <Link key={'desktopMenu' + i} className="navbar-item" to={c.path}>{c.label}</Link>
+                    ))}
+                </div>
+            </div>
+        </nav>
+    )
+}
