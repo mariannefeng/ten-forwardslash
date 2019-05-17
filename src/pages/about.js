7acdfd76470e5ed3005f8bfb3f5c86ca0c9520f2
Jessi Shank <jessishank1@gmail.com>
2019-05-17 14:33:57 -0700

    filling out about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { useRouteData } from "react-static";
+import { Box, Flex, Heading, Image, Button, Card, Text } from 'rebass'
+
+import { Section } from 'components/rebass';
 
 function About() {
     const { data } = useRouteData()
-
+    console.log("data", data)
     return (
-        <div>
-            <p>This is an about page</p>
-        </div>
+        <Section bg='minty'>
+            <Heading>{data}</Heading>
+        </Section>
     )
 }
 
