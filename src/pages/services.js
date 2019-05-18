61d786541e16ba3387282d98869face67c187638
Jessi Shank <jessishank1@gmail.com>
2019-05-18 13:17:20 -0700

    more services tweaks

--- src/pages/services.js
+++ src/pages/services.js
@@ -27,7 +27,7 @@ function Services() {
                 <FlexContent flexDirection='column' m={3} px={4} pt={4}>
                     {data.serviceSections.map((service, i) => {
                         return (
-                            <Flex bg='anotherblue' color='white' key={i} p={[4,5]} mb={4} flexDirection='column'>
+                            <Flex bg='anotherblue' color='white' key={i} p={5} mb={4} mx={1} flexDirection='column'>
                                 <Heading fontSize={4} mb={2} id={service.anchorId}>{service.name}</Heading>
                                 <Heading fontSize={2} fontFamily='mono' mb={3}>{service.tagline}</Heading>
                                 <TextNoFirstMarginP fontSize={2}><SquareListWrapper><ReactMarkdown source={service.content}/></SquareListWrapper></TextNoFirstMarginP>
