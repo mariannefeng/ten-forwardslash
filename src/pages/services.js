7cad70c08b62cd1e95834d4aa691c517e4a9762e
Jessi Shank <jessishank1@gmail.com>
2019-05-18 18:21:05 -0700

    styling links for services pageg

--- src/pages/services.js
+++ src/pages/services.js
@@ -4,11 +4,22 @@ import ReactMarkdown from "react-markdown"
 import styled from "styled-components"
 import { Flex, Heading } from 'rebass'
 
+import { colors } from '../theme'
 import { Section, FlexContent, TextNoFirstMarginP } from 'components/rebass';
 
 const SquareListWrapper = styled.div`
 ul {
     list-style-type: square;
+}
+a {
+    text-decoration: none;
+    color: ${colors.brightorange} !important;
+    &:visited {
+        color: ${colors.yellow} !important;
+    }
+    &:hover, &:active {
+        text-decoration: underline;
+    }
 }
 `
 
@@ -27,7 +38,7 @@ function Services() {
                 <FlexContent flexDirection='column' m={3} px={4} pt={4}>
                     {data.serviceSections.map((service, i) => {
                         return (
-                            <Flex bg='anotherblue' color='white' key={i} p={5} mb={4} mx={1} flexDirection='column'>
+                            <Flex bg='anotherblue' color='white' key={i} p={[4, 5]} mb={4} mx={1} flexDirection='column'>
                                 <Heading fontSize={4} mb={2} id={service.anchorId}>{service.name}</Heading>
                                 <Heading fontSize={2} fontFamily='mono' mb={3}>{service.tagline}</Heading>
                                 <TextNoFirstMarginP fontSize={2}><SquareListWrapper><ReactMarkdown source={service.content}/></SquareListWrapper></TextNoFirstMarginP>
