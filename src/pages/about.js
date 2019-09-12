2dc16cc77e54cd7f72e033368bf240b37a9dcc9f
Jessi Shank <jessishank1@gmail.com>
2019-09-12 12:24:10 -0700

    some styling on about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,17 +3,22 @@ import { useRouteData } from "react-static";
 import ReactMarkdown from "react-markdown"
 import { Box, Flex, Heading, Image, Text } from 'rebass'
 import { MEASURE_OF_A_MAN } from 'components/text-overlay-filler'
-import { Section, FullHeightFlexContent, TextNoFirstMarginP, PageTitle, OverlayText, BrandedMainHeading, BrandedSubHeading } from 'components/rebass';
+import { Section, FullHeightFlexContent, TextNoFirstMarginP, PageTitle, BrandedMainHeading, BrandedSubHeading } from 'components/rebass';
+import styled from "styled-components";
+import {colors} from "../theme";
+
+const ValuesHeader = styled(BrandedSubHeading)`
+    border-left: 0.5rem solid ${colors.green};
+    padding: 5px 1rem;
+`
 
 const AboutHeader = props =>
-    <Heading
+    <ValuesHeader
         {...props}
         fontSize={4}
-        width={[1, 1/3]}
-        py={2}
+        // py={2}
         fontFamily='mono'
-        textAlign='center'
-        color='red'
+        color='black'
         />
 const AboutBlurb = props =>
     <TextNoFirstMarginP
@@ -23,26 +28,37 @@ const AboutBlurb = props =>
         fontSize={3}
         />
 
+const Founder = styled.div`
+    border-left: 0.5rem solid ${colors.green};
+    padding: 5px 1rem;
+`
+
 function About() {
     const { data } = useRouteData()
     return (
-        <Section bg='yellow'>
+        <Section bg='white'>
 
                 <FullHeightFlexContent  px={4} flex={1} mt={3} color='black'>
                     <div style={{display: "flex", flexDirection: "column"}}>
                         <PageTitle color='black'>{`<${data.title}>`}</PageTitle>
-                        <Flex>
-                            <AboutHeader>our mission</AboutHeader>
+                        <Flex py={[2,0]}>
+                            <Flex width={[1, 1/3]} justifyContent='flex-start'>
+                                <AboutHeader>our mission</AboutHeader>
+                            </Flex>
                         </Flex>
                         <AboutBlurb>{data.mission}</AboutBlurb>
-                        <Flex>
+                        <Flex py={[2,0]}>
                             <Box width={[0, 1/3]}/>
-                            <AboutHeader>our vision</AboutHeader>
+                            <Flex width={[1, 1/3]} justifyContent={['flex-start', 'center']}>
+                                <AboutHeader>our vision</AboutHeader>
+                            </Flex>
                         </Flex>
                         <AboutBlurb>{data.vision}</AboutBlurb>
-                        <Flex>
+                        <Flex py={[2,0]}>
                             <Box width={[0, 2/3]}/>
-                            <AboutHeader>our history</AboutHeader>
+                            <Flex width={[1, 1/3]} justifyContent={['flex-start', 'flex-end']}>
+                                <AboutHeader>our history</AboutHeader>
+                            </Flex>
                         </Flex>
                         <AboutBlurb><ReactMarkdown source={data.history}/></AboutBlurb>
 
@@ -63,8 +79,10 @@ function About() {
                                             css={{objectFit: 'contain', maxHeight: '150px'}}
                                             mb={3}
                                         />
-                                        <BrandedSubHeading mb={2} fontSize={4}>{teamMember.name}</BrandedSubHeading>
-                                        <BrandedMainHeading color='red' fontFamily='mono' mb={2} fontSize={3}>{teamMember.position}</BrandedMainHeading>
+                                        <Founder>
+                                            <BrandedSubHeading mb={2} fontSize={4}>{teamMember.name}</BrandedSubHeading>
+                                            <BrandedMainHeading fontFamily='mono' mb={0} fontSize={3}>{teamMember.position}</BrandedMainHeading>
+                                        </Founder>
                                         <Text fontSize={2}><ReactMarkdown source={teamMember.bio}/></Text>
                                     </Flex>
                                 )
