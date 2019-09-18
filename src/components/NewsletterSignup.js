c8a6647bac038a5c7869b8a7ad278a818d34d68e
Jessi Shank <jessishank1@gmail.com>
2019-09-18 11:33:55 -0700

    increasing width for newsletter signup

--- src/components/NewsletterSignup.js
+++ src/components/NewsletterSignup.js
@@ -7,7 +7,7 @@ import MailingListForm from "./MailingListForm"
 
 const url = "https://ten-forward.us19.list-manage.com/subscribe/post?u=1eff7db017d8a9a0f3bc2f547&amp;id=08a107d735";
 
-const NewsletterSignup = (data) => (<FullHeightFlexContent flexDirection='column' alignItems='center' width={[5/7, 1/2, 1/4]}>
+const NewsletterSignup = (data) => (<FullHeightFlexContent flexDirection='column' alignItems='center' width={[5/7, 1/2, 1/3]}>
 
     <PageSubtitle color='black'>{data.cta}</PageSubtitle>
 
