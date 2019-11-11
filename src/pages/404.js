4783c01dbf4e33952e75ceb1284afe9003ab3fed
Marianne Feng <feng.marianne@gmail.com>
2019-11-11 12:31:18 -0800

    added downloadable links for wordpress notes docs

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default () => (
-  <div>
-    <h1>404 - Oh no's! We couldn't find that page :(</h1>
+  <div css={{textAlign: "center"}}>
+    <h1>404 - Oh no! We couldn't find that page :(</h1>
   </div>
 )
