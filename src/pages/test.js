c13830aefd2038380c15b24f6e2d43bafb9d8313
Marianne Feng <feng.marianne@gmail.com>
2020-01-17 16:48:00 -0800

    added a test page and moved my form on there so our contact page doesn't look ugly

---
+++ src/pages/test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default () => (
+    <div css={{textAlign: "center"}}>
+        <form netlify name="sad">
+            <input type="text" name="name2"/>
+            <button type="submit">submit</button>
+        </form>
+    </div>
+)
