769d51d5926c887ef89294737ca6a135671e9241
Jessi Shank <jessishank1@gmail.com>
2020-02-28 13:58:53 -0800

    cleaning up console log, un-commenting out the portfolio stuff

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -3,7 +3,6 @@ import {useRouteData} from "react-static"
 
 export default () => {
     const { data } = useRouteData()
-    console.log('data', data)
     return (
         <div css={{textAlign: "center"}}>
             <h1>this is a portfolio</h1>
