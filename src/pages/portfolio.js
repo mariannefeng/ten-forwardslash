c4801547325b8b1423c1c209b9f9f8f2d41ee431
Jessi Shank <jessishank1@gmail.com>
2020-02-28 13:27:02 -0800

    some debugging for when actually using netlify cms

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import {useRouteData} from "react-static"
 
-export default () => (
-  <div css={{textAlign: "center"}}>
-    <h1>this is a portfolio</h1>
-  </div>
-)
+export default () => {
+    const { data } = useRouteData()
+    console.log('data', data)
+    return (
+        <div css={{textAlign: "center"}}>
+            <h1>this is a portfolio</h1>
+        </div>
+    )
+}
